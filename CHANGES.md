5aaf1447998fc9894d4b9adab333bf7ada6cf90c
tinybees <a598824322@163.com>
2019-01-28 11:55:22 +0800

    添加schema_validate装饰器用于校验schema,优化exception实现方式
    Signed-off-by: tinybees <a598824322@163.com>

--- CHANGES.md
+++ CHANGES.md
@@ -1,10 +1,28 @@
 ## aeclients Changelog
 
+###[1.0.0b4] - 2019-1-28
+
+#### Added 
+- 添加schema_validate装饰器用于校验schema
+- 增加schema message 使用的消息
+#### Changed 
+- 优化exceptions的实现方式
+
+###[1.0.0b3] - 2019-1-21
+
+#### Added 
+- 增加多数据库、多实例应用方式
+- 增加在没有app时脚本中使用时的初始化功能,这样便于通用性
+- 增加错误类型，能够对错误进行定制
+- 增加单例的装饰器，修改httpclient为单例
+
+###[1.0.0b2] - 2019-1-21
+
+#### Added 
+- http基于requests的CRUD封装
+
 ###[1.0.0b1] - 2019-01-21
 
 #### Added 
-
-- http基于requests的CRUD封装
-- redis基于redis.py的CRUD封装
 - mongo基于pymongo的CRUD封装
 - 所有消息可自定义配置,否则为默认配置
--- eclients/decorators.py
+++ eclients/decorators.py
@@ -8,7 +8,15 @@
 """
 from functools import wraps
 
-__all__ = ("singleton",)
+import aelog
+import marshmallow
+from flask import g, request
+
+from .err_msg import schema_msg
+from .exceptions import FuncArgsError, HttpError
+from .utils import verify_message
+
+__all__ = ("singleton", "schema_validate")
 
 
 def singleton(cls):
@@ -29,3 +37,72 @@ def singleton(cls):
         return instances[cls_name]
 
     return _singleton
+
+
+def schema_validate(schema_obj, required: (tuple, list) = tuple(), is_extends=True, excluded: (tuple, list) = tuple(),
+                    use_zh=True, message=None):
+    """
+    校验post的json格式和类型是否正确
+    Args:
+        schema_obj: 定义的schema对象
+        required: 需要标记require的字段
+        excluded: 排除不需要的字段
+        is_extends: 是否继承schemea本身其他字段的require属性， 默认继承
+        use_zh: 消息提示是否使用中文，默认中文
+        message: 提示消息
+    Returns:
+    """
+
+    if not issubclass(schema_obj, marshmallow.Schema):
+        raise FuncArgsError(message="schema_obj type error!")
+    if not isinstance(required, (tuple, list)):
+        raise FuncArgsError(message="required type error!")
+    if not isinstance(excluded, (tuple, list)):
+        raise FuncArgsError(message="excluded type error!")
+
+    msg_zh = "msg_zh" if use_zh else "msg_en"
+    # 此处的功能保证，如果调用了多个校验装饰器，则其中一个更改了，所有的都会更改
+    if not getattr(schema_validate, "message", None) and message:
+        setattr(schema_validate, "message", verify_message(schema_msg, message or {}))
+
+    def _validated(func):
+        """
+        校验post的json格式和类型是否正确
+        """
+
+        @wraps(func)
+        async def _wrapper(*args, **kwargs):
+            """
+            校验post的json格式和类型是否正确
+            """
+            schema_message = getattr(schema_validate, "message", None)
+
+            new_schema_obj = schema_obj(unknown="EXCLUDE")
+            if required:
+                for key, val in new_schema_obj.fields.items():
+                    if key in required:  # 反序列化期间，把特别需要的字段标记为required
+                        setattr(new_schema_obj.fields[key], "required", True)
+                        setattr(new_schema_obj.fields[key], "dump_only", False)
+                    elif not is_extends:
+                        setattr(new_schema_obj.fields[key], "required", False)
+            try:
+                valid_data = new_schema_obj.load(request.json, unknown="EXCLUDE")
+                # 把load后不需要的字段过滤掉，主要用于不允许修改的字段load后过滤掉
+                for val in excluded:
+                    valid_data.pop(val, None)
+            except marshmallow.ValidationError as err:
+                # 异常退出
+                aelog.exception('Request body validation error, please check! {} {} error={}'.format(
+                    request.method, request.path, err.messages))
+                raise HttpError(400, message=schema_message[201][msg_zh], error=err.messages)
+            except Exception as err:
+                aelog.exception("Request body validation unknow error, please check!. {} {} error={}".format(
+                    request.method, request.path, str(err)))
+                raise HttpError(500, message=schema_message[202][msg_zh], error=str(err))
+            else:
+                g.json = valid_data
+            return await func(*args, **kwargs)
+
+        return _wrapper
+
+    return _validated
